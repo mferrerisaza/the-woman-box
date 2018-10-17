81acd7fb079c801352ae2d58336cd6def05b1234
Miguel Ferrer <miguef7@gmail.com>
2018-10-17 18:45:34 -0500

    remove index action and add new action for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
-  def index
-    @orders = current_user.orders
+  def new
+    @order = Order.new
   end
 
   def create
+    raise
     plan = Plan.find(params[:plan_id])
     order = Order.create!(plan_sku: plan.sku, amount: plan.price, status: 'Pendiente', user: current_user)
     redirect_to new_order_payment_path(order)

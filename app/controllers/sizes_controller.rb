5877a57f5f947467925be2045efcb7e4792cddcb
Miguel Ferrer <miguef7@gmail.com>
2018-10-17 18:46:08 -0500

    return json on sizes endpoint

--- app/controllers/sizes_controller.rb
+++ app/controllers/sizes_controller.rb
@@ -2,6 +2,8 @@ class SizesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    render json: Size.all
+    @sizes = nil # creates an anonymous scope
+    @sizes = Size.where(type_id: params[:type_id]) if params[:type_id].present?
+    render json: @sizes
   end
 end

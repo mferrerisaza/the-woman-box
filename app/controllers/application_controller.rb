4e6d0eb7dbfbcbc871eab4b0cd5d75529580b688
Miguel Ferrer <miguef7@gmail.com>
2018-10-18 18:38:34 -0500

    save order on session if user isnt logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if session[:order].present?
+      create_order_after_user_sign_up(current_user)
+    else
+      super
+    end
+  end
+
+  def create_order_after_user_sign_up(user)
+    order = Order.new(session[:order])
+    @order.user = user
+    session[:order] = nil
+    if @order.save
+    else
+    end
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,13 @@ class OrdersController < ApplicationController
 
   def create
     plan = Plan.find(order_params[:plan_id])
-    order = Order.create!(plan_sku: plan.sku, amount: plan.price, status: 'Pendiente', user: current_user)
-    redirect_to new_order_payment_path(order)
+    if current_user
+      Order.create!(plan_sku: plan.sku, amount: plan.price, status: 'Pendiente', user: current_user)
+      redirect_to new_order_payment_path(order)
+    else
+      session[:order] = { plan_sku: plan.sku, amount_cents: plan.price_cents, status: 'Pendiente' }
+      redirect_to new_user_registration_path
+    end
   end
 
   private

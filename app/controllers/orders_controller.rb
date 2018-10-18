8d07bb9e3c6b7735e7681614e64b913c8f22f4e3
Miguel Ferrer <miguef7@gmail.com>
2018-10-18 18:14:02 -0500

    cleaning

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def new
     @order = Order.new
   end
 
   def create
-    raise
-    plan = Plan.find(params[:plan_id])
+    plan = Plan.find(order_params[:plan_id])
     order = Order.create!(plan_sku: plan.sku, amount: plan.price, status: 'Pendiente', user: current_user)
     redirect_to new_order_payment_path(order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:plan_id)
   end
 end

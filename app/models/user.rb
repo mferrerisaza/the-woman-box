8106f8895ed137d4e1456e6d00a24d169d89f92c
Miguel Ferrer <miguef7@gmail.com>
2018-10-19 16:14:59 -0500

    epayco documents type

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+  DOC_TYPE = %w[NIT CC CE TI PPN SSN LIC DNI]
 
   def full_name
     "#{first_name} #{last_name}".split.map(&:capitalize).join(' ')

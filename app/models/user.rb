be3fb682efda9b2ee5eecbd0b038b3863aa653b5
Miguel Ferrer <miguef7@gmail.com>
2018-10-18 21:01:02 -0500

    cleaning

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
   def full_name
     "#{first_name} #{last_name}".split.map(&:capitalize).join(' ')
   end

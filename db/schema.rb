792be1be0bf1e38be899349f9c5a36f2a6680152
Miguel Ferrer <miguef7@gmail.com>
2018-10-18 19:18:52 -0500

    add delivery date to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_001137) do
+ActiveRecord::Schema.define(version: 2018_10_19_001533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_001137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "plan_sku"
+    t.string "delivery_date"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 

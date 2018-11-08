e52d2c1e5b01a08759a83b61b191b1d8fed25da8
Miguel Ferrer <miguef7@gmail.com>
2018-11-08 18:56:36 -0500

    mailjet provisional sender email

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,7 +2,7 @@
 Mailjet.configure do |config|
   config.api_key = ENV['MAIL_JET_API_KEY']
   config.secret_key = ENV['MAIL_JET_SECRET_KEY']
-  config.default_from = "hola@thewomenbox.com"
+  config.default_from = "miguef7@gmail.com"
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.

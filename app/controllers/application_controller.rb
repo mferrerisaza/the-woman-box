a4f43948263cc9d8910ef80b669b9de3a984c401
Daniel Lopez <dlopez.10g@gmail.com>
2018-11-08 11:36:11 -0500

    default url options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?

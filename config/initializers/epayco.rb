6537872f9829baeee1f78e0999e6d999c97dcd83
Miguel Ferrer <miguef7@gmail.com>
2018-11-19 11:39:57 -0500

    prevent epayco error to happen again

--- config/initializers/epayco.rb
+++ config/initializers/epayco.rb
@@ -3,4 +3,8 @@ require 'epayco-ruby'
 Epayco.apiKey = ENV['EPAYCO_PUBLIC_KEY']
 Epayco.privateKey = ENV['EPAYCO_PRIVATE_KEY']
 Epayco.lang = 'ES'
-Epayco.test = false
+if Rails.env.production?
+  Epayco.test = false
+else
+  Epayco.test = true
+end

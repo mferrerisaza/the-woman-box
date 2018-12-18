1d4bb18f1694b634e967fc597494609654657e4e
Daniel Lopez <dlopez.10g@gmail.com>
2018-12-18 13:02:16 -0500

    specs for order instance methods (delivery dates and double box)

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -26,4 +26,29 @@ RSpec.describe Order, type: :model do
       expect(order.errors.messages).to have_key(:user)
     end
   end
+
+  context "instance methods" do
+    it "returns the correct delivery date" do
+      expect(order.delivery_date).to eq Date.parse("2018-12-30")
+    end
+
+    it "returns teh correct delivery date when 1st of month" do
+      order.update(created_at: "2018-12-01")
+      expect(order.delivery_date).to eq Date.parse("2018-12-20")
+    end
+
+    it "returns teh correct delivery date when last day of month" do
+      order.update(created_at: "2018-11-30")
+      expect(order.delivery_date).to eq Date.parse("2018-12-10")
+    end
+
+    it "return false if no double box" do
+      expect(order.double_box?).to eq false
+    end
+
+    it "return true if double_box" do
+      order.update(last_period: "2018-12-03")
+      expect(order.double_box?).to eq true
+    end
+  end
 end

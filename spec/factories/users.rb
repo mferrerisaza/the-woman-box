db77ca7205969bf0599c3d83383691bc63dd89af
Daniel Lopez <dlopez.10g@gmail.com>
2018-12-18 13:01:29 -0500

    change on email on user factory bot, decía que ya estaba en uso

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "test#{n}@gmail.com" }
+    sequence(:email) { |n| "test1#{n}@gmail.com" }
     password "123456"
     first_name "Miguel"
     last_name "Ferrer"

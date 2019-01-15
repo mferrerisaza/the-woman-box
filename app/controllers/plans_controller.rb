21e5a84d036bcf9184bcc6bc8f2d1b5e3467061b
Miguel Ferrer <miguef7@gmail.com>
2019-01-14 20:15:42 -0500

    modify plan index action to take into account the plan type when showing options

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,8 +3,9 @@ class PlansController < ApplicationController
   skip_after_action :verify_authorized, :verify_policy_scoped
 
   def index
-    @plans = nil # creates an anonymous scope
-    @plans = Plan.where(size_id: params[:size_id]) if params[:size_id].present?
+    @plans = Plan.where(nil) # creates an anonymous scope
+    @plans = @plans.where(size_id: params[:size_id]) if params[:size_id].present?
+    @plans = @plans.where(billing_type: params[:plan_type]) if params[:plan_type].present?
     render json: @plans
   end
 

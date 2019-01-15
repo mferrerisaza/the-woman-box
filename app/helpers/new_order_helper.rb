0fa7e17ee126161769db4f3a3674fcdf627ceb68
Miguel Ferrer <miguef7@gmail.com>
2019-01-15 15:20:21 -0500

    make more texts dynamic depeding on the plan type

--- app/helpers/new_order_helper.rb
+++ app/helpers/new_order_helper.rb
@@ -10,4 +10,40 @@ module NewOrderHelper
       return "NUESTRO PLAN DE 6 MESES"
     end
   end
+
+  def plan_price(plan_type)
+    if plan_type == "Recurrente"
+      return "$34.900 al mes"
+    elsif plan_type == "1Mes"
+      return "$34.900"
+    elsif plan_type == "3Meses"
+      return "$33.167 al mes"
+    elsif plan_type == "6Meses"
+      return "$31.417 al mes"
+    end
+  end
+
+  def order_description(plan_type)
+    if plan_type == "Recurrente"
+      return "Suscripción mensual a The Women Box"
+    elsif plan_type == "1Mes"
+      return "1 mes de suscripción a The Women Box"
+    elsif plan_type == "3Meses"
+      return "3 meses de suscripción a The Women Box"
+    elsif plan_type == "6Meses"
+      return "6 meses de suscripción a The Women Box"
+    end
+  end
+
+  def order_delivery_date(plan_type)
+    if plan_type == "Recurrente"
+      return "de cada mes"
+    elsif plan_type == "1Mes"
+      return "del mes"
+    elsif plan_type == "3Meses"
+      return "de cada mes"
+    elsif plan_type == "6Meses"
+      return "de cada mes"
+    end
+  end
 end

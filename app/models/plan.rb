dbcf657d9b518c93a4d710a226976471e18b4a7b
Miguel Ferrer <miguef7@gmail.com>
2019-01-14 19:46:23 -0500

    add billing_type to plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,6 @@
 class Plan < ApplicationRecord
+  enum billing_type: %w[Recurrente 1Mes 3Meses 6Meses]
+
   monetize :price_cents
   belongs_to :size
   validates :sku, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_153547) do
+ActiveRecord::Schema.define(version: 2019_01_15_003814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_153547) do
     t.string "sku"
     t.bigint "size_id"
     t.string "photo"
+    t.integer "billing_type"
     t.index ["size_id"], name: "index_plans_on_size_id"
   end
 

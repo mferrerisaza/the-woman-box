8716b6cd5c8fb75e0e30921bb577a847e70ecdcf
Miguel Ferrer <miguef7@gmail.com>
2019-01-14 20:41:24 -0500

    cleaning

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,7 +4,6 @@ class PlansController < ApplicationController
 
   def index
     @plans = Plan.where(nil) # creates an anonymous scope
-    # @plans = @plans.where(size_id: params[:size_id]) if params[:size_id].present?
     if params[:plan_type].present? && params[:size_id].present?
       @plans = @plans.where(size_id: params[:size_id]).where(billing_type: params[:plan_type])
     else

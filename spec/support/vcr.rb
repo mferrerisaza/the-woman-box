ddb621332dbf9c7ed961d92935d3529c49400937
Miguel Ferrer <miguef7@gmail.com>
2019-01-16 19:33:41 -0500

    Encrypt epayco keys on cassetes and remove cassetes from gitignroe, change re_record interval for cassetes to 1 day

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,4 +5,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
+  config.default_cassette_options = { re_record_interval: 1.day }
+  config.filter_sensitive_data('<surprise1>') { ENV['EPAYCO_PUBLIC_KEY'] }
+  config.filter_sensitive_data('<surprise2>') { ENV['EPAYCO_PRIVATE_KEY'] }
 end

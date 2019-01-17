583abc71cd4cd596926236f25fe7008037d274ce
Miguel Ferrer <miguef7@gmail.com>
2019-01-16 23:15:16 -0500

    update job to make it work with non suscription orders

--- app/jobs/update_order_status_job.rb
+++ app/jobs/update_order_status_job.rb
@@ -11,9 +11,21 @@ class UpdateOrderStatusJob < ApplicationJob
         subscription = Epayco::Subscriptions.get subscription_id
         subscription_status = subscription[:status_plan]
         order.update(status: order.epayco_status_transform(subscription_status))
+      else
+        update_status_on_one_off_payments(order)
       end
     end
     # Para consultar el estado de una transaccion
     # Epayco::Charge.get "numero de factura que llega al email"[:x_response]
+  end
+
+  def update_status_on_one_off_payments(order)
+    if order.status == "Pendiente"
+      transaction_id = JSON.parse(order.payment)["data"]["x_ref_payco"].to_s
+      transaction = Epayco::Charge.get transaction_id
+      transaction_status = transaction[:data][:x_response]
+      order.update(status: order.epayco_status_transform(transaction_status))
+    end
+    order.update(status: "Inactiva") if order.remaining_active_days <= 0 && order.status == "Pagada"
   end
 end
